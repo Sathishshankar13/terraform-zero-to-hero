13a6b0d694cfc90bba1ccbb9c5dddc9404462b4a
Sathish Shankar <168322001+Sathishshankar13@users.noreply.github.com>
2024-06-11 00:37:57 -0400

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  ami_value = "ami-04b70fa74e45c3917" # replace this
   instance_type_value = "t2.micro"
   subnet_id_value = "subnet-01518b6d89451d9d4". # replace this
 }
