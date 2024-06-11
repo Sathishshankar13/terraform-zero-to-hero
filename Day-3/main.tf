19ef49bf3b8759b84fcfe6c0586ee44c62e25eae
Sathish Shankar <168322001+Sathishshankar13@users.noreply.github.com>
2024-06-11 00:40:10 -0400

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -6,5 +6,5 @@ module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-04b70fa74e45c3917" # replace this
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-01518b6d89451d9d4". # replace this
+  subnet_id_value = "subnet-01518b6d89451d9d4"
 }
